3dcfd8ea883087b42d951bec621a6d179942465d
joonho0129 <756dtkdtng@privaterelay.appleid.com>
2026-07-08 00:09:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def analyze():
     return render_template('index.html', result=result, review=review)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=5001)
